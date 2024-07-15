d4335da6de03329adbabdd0711ac89fb491ad0d6
Sanilal P Sathyan <sanilal@gmail.com>
2024-07-15 17:48:48 +0400

    contact added

--- components/BookNow.jsx
+++ components/BookNow.jsx
@@ -1,13 +1,15 @@
 import bgImage from "@/public/assets/bg.png";
 import ContactForm from "./ContactForm";
+import bookImg from "@/public/assets/booknow.png"
+import Image from "next/image";
 
 const BookNow = () => {
   return (
-    <div style={{ backgroundImage: `url(${bgImage.src})` }} className="w-full h-auto bg-cover bg-center">
+    <div style={{ backgroundImage: `url(${bgImage.src})` }} className="w-full h-auto bg-cover bg-center flex justify-center">
       {/* Content goes here */}
-      <div className='container justify-center flex'>
+      <div className='container justify-center flex py-12'>
         <div className="w-full grid grid-cols-2">
-            <div>sdffsd</div>
+            <div className='flex justify-center items-center'><Image src={bookImg} alt='' width={200} /></div>
             <div><ContactForm /></div>
         </div>
       </div>
--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
-
 const ContactForm = () => {
   return (
-    <div>ContactForm</div>
-  )
+    <div className="flex items-center justify-center"
+    >
+      <form className="p-2 rounded-md w-full max-w-lg">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+          <div className="flex flex-col">
+            <input
+              type="text"
+              id="name"
+              className="p-2 bg-none border border-blue rounded"
+              placeHolder="Name"
+            />
+          </div>
+          <div className="flex flex-col">
+            <input
+              type="text"
+              id="business-name"
+              className="p-2 bg-transparent border border-blue rounded"
+              placeHolder="Business Name"
+            />
+          </div>
+          <div className="flex flex-col">
+            <input
+              type="text"
+              id="contact-number"
+              className="p-2 bg-transparent border border-blue rounded"
+              placeHolder="Contact Number"
+            />
+          </div>
+          <div className="flex flex-col">
+            <input
+              type="email"
+              id="email-id"
+              className="p-2 bg-transparent border border-blue rounded"
+              placeHolder="Email ID"
+            />
+          </div>
+          <div className="flex flex-col sm:col-span-2">
+            <input
+              type="text"
+              id="services-interested"
+              className="p-2 bg-transparent border border-blue rounded"
+              placeHolder='Services Interested'
+            />
+          </div>
+          <div className="flex flex-col sm:col-span-2">
+            <textarea
+              id="your-message"
+              className="p-2 bg-transparent border border-blue rounded h-32" placeHolder='Your Message'
+            ></textarea>
+          </div>
+          <div className="flex justify-left sm:col-span-2">
+            <button
+              type="submit"
+              className="bg-primary text-white py-2 px-6 rounded-md hover:bg-pink-700 transition"
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default ContactForm
+export default ContactForm;
